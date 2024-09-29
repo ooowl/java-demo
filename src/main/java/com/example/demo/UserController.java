3d4a5622cfa994eb6d3b36bf785082f9020dbb32
owl <wangxiaopeng@surfilter.com>
2024-09-29 16:09:01 +0800

    jpa

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// http://localhost:8080/users
 @RestController
 @RequestMapping("/users")
 public class UserController {
