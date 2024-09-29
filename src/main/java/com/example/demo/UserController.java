7a3b8bedb12d1b12b2543e125d9f71925f974453
owl <wangxiaopeng@surfilter.com>
2024-09-29 16:08:29 +0800

    jpa

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{name}")
+    public List<User> getUserByName(@RequestParam("name") String name) {
+        return userRepository.findByName(name);
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // PUT http://localhost:8080/users/32
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        ResponseEntity<User> userResponseEntity = userRepository.findById(id)
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
+        return userResponseEntity;
+    }
+
+    // PUT http://localhost:8080/users/32
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        ResponseEntity<User> userResponseEntity = userRepository.findById(id)
+                .map(user -> {
+                    user.setName(userDetails.getName());
+                    user.setEmail(userDetails.getEmail());
+                    User updatedUser = userRepository.save(user);
+                    return ResponseEntity.ok(updatedUser);
+                })
+                .orElse(ResponseEntity.notFound().build());
+        return userResponseEntity;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    userRepository.delete(user);
+                    return ResponseEntity.noContent().<Void>build(); // 返回 204 No Content
+                })
+                .orElse(ResponseEntity.notFound().build()); // 返回 404 Not Found
+    }
+
+}
